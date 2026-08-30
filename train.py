17f6732d4790a98553fe9f1d73fa2914aa4d732a
Juan Gomez <juan.gomez44@usc.edu.co>
2026-08-30 12:37:33 -0500

    refactor: update training loop parameters and hyperparameter configuration in train.py

--- train.py
+++ train.py
@@ -32,8 +32,8 @@ from rsl_rl.runners import OnPolicyRunner
 EXPERIMENT_NAME = "bipedo-usc-v1"
 
 parser = argparse.ArgumentParser(add_help=True)
-parser.add_argument("-n", "--num_envs",        type=int,   default=2)
-parser.add_argument("--max_iterations",        type=int,   default=100)
+parser.add_argument("-n", "--num_envs",        type=int,   default=4096)
+parser.add_argument("--max_iterations",        type=int,   default=2000)
 parser.add_argument("-d", "--device",          type=str,   default="gpu")
 parser.add_argument("-e", "--exp_name",        type=str,   default=EXPERIMENT_NAME)
 parser.add_argument("--resume",                action="store_true")
